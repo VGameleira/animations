8d5311cf8c4b7aad36e678a64db650d26d24dfae
VGameleira.Dev <vinigameleira.stos@gmail.com>
2025-06-04 17:28:45 -0300

    start

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('animacao2').addEventListener('click', animar);
+
+  function animar() {
+    const elemento = document.getElementById('animacao2');
+    elemento.classList.toggle('clicou');
+  }
+});
